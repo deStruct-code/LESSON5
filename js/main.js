c0ea4a9e9e315d9a9806db834dd09e573b5f8c25
Vladimir Konyukhoff <vkonyukhoff@gmail.com>
2025-02-04 00:39:08 +0300

    it works

--- js/main.js
+++ js/main.js
@@ -1,13 +1,35 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    // loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    //   type: 'fraction', 
+
+    breakpoints: {
+      320: 
+      {enabled: true,} ,
+      768: 
+      { enabled: false, }
     },
-  
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    
+    spaceBetween: 16,
+    slidesPerView: 'auto',
+    
   });
-  
+
+const readMore = document.querySelector('.read-more');
+const hideMore = document.querySelector('.hide-more');
+const brandList = document.querySelector('.brandList');
+
+
+readMore.addEventListener('click', function (e) {
+    brandList.style.height = '100%';
+    readMore.style.display = 'none';
+    hideMore.style.display = 'inline-block';  
+});
+
+hideMore.addEventListener('click', function(e){
+    brandList.style.height = '176px';
+    readMore.style.display = 'inline-block';
+    hideMore.style.display = 'none';
+});
