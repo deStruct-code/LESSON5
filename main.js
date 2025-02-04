54ac13f44bc69df5bee57437e2e81d59d3c7df95
Vladimir Konyukhoff <vkonyukhoff@gmail.com>
2025-02-04 20:07:42 +0300

    vlad helps me

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ const swiper = new Swiper('.swiper', {
       320: 
       {enabled: true,} ,
       768: 
-      { enabled: false, }
+      {enabled: false, }
     },
 
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+        
     },
     
     spaceBetween: 16,
@@ -17,19 +18,21 @@ const swiper = new Swiper('.swiper', {
     
   });
 
+const width = window.screen.width;
 const readMore = document.querySelector('.read-more');
 const hideMore = document.querySelector('.hide-more');
 const brandList = document.querySelector('.brandList');
 
 
-readMore.addEventListener('click', function (e) {
-    brandList.style.height = '100%';
-    readMore.style.display = 'none';
-    hideMore.style.display = 'inline-block';  
+
+readMore.addEventListener('click', function(e){
+  brandList.classList.add('brandList__full');
+  readMore.classList.add('hidden');
+  hideMore.classList.remove('hidden');
 });
 
 hideMore.addEventListener('click', function(e){
-    brandList.style.height = '176px';
-    readMore.style.display = 'inline-block';
-    hideMore.style.display = 'none';
+    brandList.classList.remove('brandList__full');
+    readMore.classList.remove('hidden');
+    hideMore.classList.add('hidden');
 });
